453ef14d1aaff5976cf7b251cfdd0be31ad8447f
crollins18 <belac2002@gmail.com>
2019-12-22 21:18:56 -0500

    edit form search

--- src/main/SearchServlet.java
+++ src/main/SearchServlet.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/SearchServlet")
 public class SearchServlet extends HttpServlet {
 	
-	private String teamID;
+	private String teamID = "";
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -33,6 +33,36 @@ public class SearchServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		PrintWriter writer = response.getWriter();
+		processScores s = new processScores();
+		writer.println("<html>");
+		writer.println("<head><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"><meta http-equiv=\"refresh\" content=\"30\"></head>");
+		writer.println("<body>");
+		writer.println("<h2>Showing Team Detail for " + teamID + " as of " + new Date() + "</h2>");
+		writer.println("<h3>Fetching from " + s.getBase() + "</h3>");
+		writer.println("<hr>");
+		writer.println("<table border=\"4\" class='CSSTableGenerator'>");
+		ArrayList<String[]> temp = s.getTeamboard(teamID);
+		for(int r=0; r<2; r++) {
+			writer.println("<tr>");
+			for(int c=0; c<temp.get(0).length; c++) {
+				writer.print("<td>"+temp.get(r)[c]+"</td>");
+			}
+			writer.println("</tr>");
+		}
+		writer.println("</table>");
+		writer.println("<table border=\"4\" class='CSSTableGenerator'>");
+		writer.println("<br>");
+		for(int r=2; r<temp.size(); r++) {
+			writer.println("<tr>");
+			for(int c=0; c<temp.get(2).length; c++) {
+				writer.print("<td>"+temp.get(r)[c]+"</td>");
+			}
+			writer.println("</tr>");
+		}
+		writer.println("</table>");
+		writer.println("</body>");
+		writer.println("</html>");
 	}
 
 	/**
@@ -46,7 +76,7 @@ public class SearchServlet extends HttpServlet {
 		writer.println("<html>");
 		writer.println("<head><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"><meta http-equiv=\"refresh\" content=\"30\"></head>");
 		writer.println("<body>");
-		writer.println("<h2>Showing Team Detail as of " + new Date() + "</h2>");
+		writer.println("<h2>Showing Team Detail for " + teamID + " as of " + new Date() + "</h2>");
 		writer.println("<h3>Fetching from " + s.getBase() + "</h3>");
 		writer.println("<hr>");
 		writer.println("<table border=\"4\" class='CSSTableGenerator'>");
